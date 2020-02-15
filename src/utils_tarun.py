c804bf747a83ca2968dd1be2561fc640e49f75df
tarun tiwari <git config --global user.email taruntiwaritt1997@gmail.com>
2020-02-16 03:26:11 +0530

    updated utils

--- src/utils_tarun.py
+++ src/utils_tarun.py
@@ -4,24 +4,25 @@ from flask import Flask, request, url_for
 def get_data():
 	if request.method == "POST":
 		formDictionary = {}
-    	formDictionary['cin']=request.form['cin']
-    	formDictionary['company_name']=request.form['company']
-    	formDictionary['date_of_registration']=request.form['registration_date']
-    	formDictionary['state']=request.form['state']
-    	formDictionary['roc']=request.form['roc']
-    	formDictionary['company_status']=request.form['status']
-    	formDictionary['category']=request.form['category']
-    	formDictionary['class']=request.form['class']
-    	formDictionary['authorized_capital']=request.form['auth']
-    	formDictionary['paidup_capital']=request.form['paid']
-    	formDictionary['activity_code']=request.form['code']
-    	formDictionary['activity_description']=request.form['desc']
-    	formDictionary['registered_office_address']=request.form['address']
-    	formDictionary['email']=request.form['email']
+    	formDictionary['CIN']=request.form['cin']
+    	formDictionary['COMPANY NAME']=request.form['company']
+    	formDictionary['DATE OF REGISTRATION']=request.form['registration_date']
+    	formDictionary['STATE']=request.form['state']
+    	formDictionary['ROC']=request.form['roc']
+    	formDictionary['COMPANY STATUS']=request.form['status']
+    	formDictionary['CATEGORY']=request.form['category']
+    	formDictionary['CLASS']=request.form['class']
+        formDictionary['COMPANY TYPE']='Non-government'
+    	formDictionary['AUTHORIZED CAPITAL']=request.form['auth']
+    	formDictionary['PAIDUP CAPITAL']=request.form['paid']
+    	formDictionary['ACTIVITY CODE']=request.form['code']
+    	formDictionary['ACTIVITY DESCRIPTION']=request.form['desc']
+    	formDictionary['REGISTERED OFFICE ADDRESS']=request.form['address']
+    	formDictionary['EMAIL']=request.form['email']
         
         
         month = formDictionary['date_of_registration'][:2]
         month_dictionary = {'01':'January','02':'February','03':'March','04':'April','05':'May','06':'June',
         '07':'July','08':'August','09':'September','10':'October','11':'November','12':'December'}
-        formDictionary['month_name']=month_dictionary[month]
+        formDictionary['MONTH NAME']=month_dictionary[month]
         
